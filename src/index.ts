098ca98d5fa5874f7bb271914588f651b3bd92c2
Rahul Thakoor <rahul@balena.io>
2021-12-17 08:26:13 +0400

    Generate Dockefile
    Change-type: patch
    Signed-off-by: Rahul Thakoor <rahul@balena.io>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,74 @@
 import * as _ from 'lodash';
+import * as fs from "fs";
 
 import { createOutputDir, readInput, writeOutputs } from './transformer';
 
-console.log('Template Transformer starting');
+console.log('t-buildroot-kconfig-to-oci-source Started');
+
+// const generateCustomConf = () => {}
+
+const generateDockerfile = async (buildrootVersion="2021.02.4") => {
+	let dockerfile = `
+	FROM balenalib/amd64-debian:buster-run  AS buildroot-base
+
+	RUN install_packages \
+			bc \
+			build-essential \
+			ca-certificates \
+			cmake \
+			cpio \
+			file \
+			locales \
+			python3 \
+			rsync \
+			unzip \
+			wget
+
+	RUN sed -i 's/# \(en_US.UTF-8\)/\1/' /etc/locale.gen && \
+		/usr/sbin/locale-gen && \
+		useradd -ms /bin/bash br-user && \
+		chown -R br-user:br-user /home/br-user
+
+	USER br-user
+
+	WORKDIR /home/br-user
+
+	ENV LC_ALL=en_US.UTF-8
+
+	ARG BR_VERSION=${buildrootVersion}
+
+	SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+
+	RUN wget -q -O- https://buildroot.org/downloads/buildroot-$BR_VERSION.tar.gz | tar xz --strip 1
+
+	
+	FROM buildroot-base as rootfs
+
+	ARG ROOTFS_LIBC=glibc
+
+	COPY custom_conf.cfg ./
+
+	RUN support/kconfig/merge_config.sh -m \
+		custom_conf.cfg
+
+	RUN make olddefconfig && make source && make
+
+	USER root
+
+	WORKDIR /rootfs
+
+	RUN tar xpf /home/br-user/output/images/rootfs.tar -C /rootfs
+
+	# Final image
+	FROM scratch
+
+	COPY --from=rootfs rootfs/ /
+
+	SHELL ["/bin/sh", "-o", "pipefail", "-c"]
+	`
+
+	return dockerfile;
+}
 
 const run = async () => {
 	const input = await readInput();
@@ -11,15 +77,18 @@ const run = async () => {
 	const outputDir = await createOutputDir();
 	console.log('output directory:', outputDir);
 
-	// TODO your code goes here. You can
-	// - inspect the input contract
-	// - process the artifact that you can find in the artifactPath
-	//   - note that the input folder is read-only!
-	// - produce some new artifact and place it in `outputDir`
+	// create Dockerfile in outputDir
+	let dockerfile = await generateDockerfile();
+	console.log(dockerfile)
+	await fs.promises.writeFile(`${outputDir}/Dockerfile`, dockerfile);
+
+
 	const outContract = {
-		type: 'type-my-out-type@1.2.3',
+		type: 'type-product-os-t-service-source@1.1.1',
 		data: {
-			someResultProperty: 1,
+			fragment: {
+				type: 'image@1.0.0'
+			},
 		},
 	};
 
